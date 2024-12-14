97d2ca4de0557279ee91f672bf1447150c4b949a
cloudiandevops <cloudiandevops@gmail.com>
2024-12-15 03:30:43 +0530

    s3-bucket config updated

--- terraform-aws/s3-backend/s3-bucket.tf
+++ terraform-aws/s3-backend/s3-bucket.tf
@@ -1,16 +1,27 @@
 # Create S3 Bucket
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-aws-ci-state-unique" # Replace with a globally unique name
-  
-  tags = {
-    Name        = "TerraformStateBucket"
-    Environment = "Prod"
-  }
-  lifecycle {
-    prevent_destroy = false
-  }
-
+data "aws_s3_bucket" "existing_bucket" {
+  bucket = "terraform-aws-ci-state-unique"
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  count  = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? 1 : 0
+  bucket = "terraform-aws-ci-state-unique"
+  force_destroy = true
+}
+
+
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "terraform-aws-ci-state-unique" # Replace with a globally unique name
+  
+#   tags = {
+#     Name        = "TerraformStateBucket"
+#     Environment = "Prod"
+#   }
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+
+# }
 
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.terraform_state.id
@@ -19,3 +30,4 @@ resource "aws_s3_bucket_versioning" "versioning" {
     status = "Enabled"
   }
 }
+
