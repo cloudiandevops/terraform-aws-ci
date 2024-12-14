a30b9e65cba9358a37fbd8e1c104423261a3711d
cloudiandevops <cloudiandevops@gmail.com>
2024-12-15 03:44:22 +0530

    build issue

--- terraform-aws/s3-backend/s3-bucket.tf
+++ terraform-aws/s3-backend/s3-bucket.tf
@@ -42,8 +42,10 @@ data "aws_s3_bucket" "existing_bucket" {
   # Use the actual bucket name here
 }
 
-# Create the bucket only if it does not exist
+# Create the S3 bucket only if it doesn't already exist
 resource "aws_s3_bucket" "terraform_state" {
+  count = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? 1 : 0
+
   bucket = "terraform-aws-ci-state-unique"
   tags = {
     Environment = "CI"
@@ -53,13 +55,12 @@ resource "aws_s3_bucket" "terraform_state" {
   lifecycle {
     prevent_destroy = false
   }
-
-  # Conditional creation based on whether the bucket already exists
-  count = data.aws_s3_bucket.existing_bucket.id != "" ? 0 : 1
 }
 
 # Enable versioning for the bucket if it was created
 resource "aws_s3_bucket_versioning" "versioning" {
+  count = length(aws_s3_bucket.terraform_state) > 0 ? 1 : 0
+
   bucket = aws_s3_bucket.terraform_state[0].id
 
   versioning_configuration {
